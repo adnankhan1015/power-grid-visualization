91316ba472fbfea4b18899a5f63bc523bb4f74a7
Adnan Khan <adnan.khan2@kyndryl.com>
2023-08-20 21:52:37 +0500

    Create hook and test hook for powerGridInfo

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { usePowerGridInfo } from "../hooks";
 
 const ChartContainer = () => {
-  return <div>Chart Container</div>;
+  const { isError, isLoading, colorMap, data: chartData } = usePowerGridInfo();
+
+  return (
+    <>
+      {isLoading ? (
+        <>Loading...</>
+      ) : isError ? (
+        <>Sorry! Couldn't fetch record</>
+      ) : (
+        <div id="chart-container" style={{ width: "100%", height: "500px" }} />
+      )}
+    </>
+  );
 };
 
 export default ChartContainer;
---
+++ src/hooks/index.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { getPowerGridData } from "../api/data-grid.service";
+
+export const usePowerGridInfo = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(undefined);
+
+  const colorMap = {
+    Main: "#B798F5",
+    Solar: "#02E10C",
+    DG: "#403F3D",
+    Battery: "#FDE602",
+    "Solar+Battery": "#86B0FF",
+    "Battery+Solar": "#86B0FF",
+    "Main+Solar": "#7243D0",
+    "Main+Battery": "#32864B",
+    "Main+Solar+Battery": "#8BC486",
+    "DG+Battery": "magenta",
+    "DG+Solar+Battery": "cyan",
+    "DG+Battery+Solar": "cyan",
+    Undetermined: "#BBE3FD",
+    "Missing Data": "white",
+  };
+
+  const getChartData = async () => {
+    setIsLoading(true);
+    const apiData = await getPowerGridData();
+    if (apiData !== null) {
+      setData(apiData?.data);
+    } else {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
+
+  // When component renders
+  useEffect(() => {
+    getChartData();
+  }, []);
+
+  return {
+    isLoading,
+    isError,
+    data,
+    colorMap,
+  };
+};
